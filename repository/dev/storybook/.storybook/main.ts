6d092838618665a175711261135350ccf1ffed6e
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-11-18 22:46:05 +0900

    chore(workspace): storybook 위치 dev 폴더 밑으로 이동

--- repository/dev/storybook/.storybook/main.ts
+++ repository/dev/storybook/.storybook/main.ts
@@ -1,6 +1,6 @@
-import { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from "@storybook/nextjs";
 
-import { join, dirname } from "path";
+import { join, dirname } from "node:path";
 
 function getAbsolutePath(value: string): string {
   return dirname(require.resolve(join(value, "package.json")));
@@ -9,7 +9,7 @@ function getAbsolutePath(value: string): string {
 const config: StorybookConfig = {
   stories: [
     "../../../apps/request-client/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../../ui-kit/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../../../packages/ui-kit/src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [getAbsolutePath("@storybook/addon-essentials")],
   framework: {
