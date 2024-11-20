d888bceb7ded2a028e488555642efdab89009994
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-11-20 12:59:07 +0900

    fix(workspace): ui-kit 제거 및 client에서 shadcn 관리

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -1,27 +1,33 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "../../packages/ui-kit/dist/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "../../packages/ui-kit/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        kakao: "#FEE500",
-        selected: "#787878",
-        "not-selected": "#D9D9D9",
-      },
-      fontSize: {
-        caption: "8px",
-      },
-      screens: {
-        "2xl": "1512px",
-      },
-      maxWidth: {
-        "2xl": "1512px",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+  			kakao: '#FEE500',
+  			selected: '#787878',
+  			'not-selected': '#D9D9D9'
+  		},
+  		fontSize: {
+  			caption: '8px'
+  		},
+  		screens: {
+  			'2xl': '1512px'
+  		},
+  		maxWidth: {
+  			'2xl': '1512px'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
