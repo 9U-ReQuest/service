51d06952d11136e1ba364f607b8b932e46903ef3
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-11-20 14:19:30 +0900

    chore(workspace): pr template 위치 변경 및 workspace 코드 포매팅

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -1,32 +1,32 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "../../packages/ui-kit/dist/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)',
-  			kakao: '#FEE500',
-  			selected: '#787878',
-  			'not-selected': '#D9D9D9'
-  		},
-  		fontSize: {
-  			caption: '8px'
-  		},
-  		screens: {
-  			'2xl': '1512px'
-  		},
-  		maxWidth: {
-  			'2xl': '1512px'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        kakao: "#FEE500",
+        selected: "#787878",
+        "not-selected": "#D9D9D9",
+      },
+      fontSize: {
+        caption: "8px",
+      },
+      screens: {
+        "2xl": "1512px",
+      },
+      maxWidth: {
+        "2xl": "1512px",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
