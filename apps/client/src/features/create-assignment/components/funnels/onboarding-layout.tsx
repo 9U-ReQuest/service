c662b9a2ac946eb0919ff57da8f0049b1c9b415b
mj <minai621@naver.com>
2024-11-23 02:52:02 +0900

    feat(client): 온보딩 퍼널 구현

---
+++ apps/client/src/features/create-assignment/components/funnels/onboarding-layout.tsx
@@ -0,0 +1,45 @@
+import logo from "@/assets/icons/logo.svg";
+import InputImage from "@/assets/images/input-1.png";
+import Typography from "@/shared/ui/common/typography/typography";
+import Flex from "@/shared/ui/wrapper/flex/flex";
+import Image from "next/image";
+import type { PropsWithChildren } from "react";
+
+export default function OnboardingLayout({ children }: PropsWithChildren) {
+  return (
+    <Flex className="w-full justify-center">
+      <Flex direction="col" className="max-w-[1200px] w-full">
+        <Flex justifyContent="center" className="py-4">
+          <Image src={logo} alt="ReQuest Logo" width={200} height={50} />
+        </Flex>
+
+        <main className="w-full">
+          <Flex direction="row" className="container mx-auto px-4 py-8">
+            <Flex direction="col" alignItems="center" className="flex-[0_0_60%]">
+              <Image
+                src={InputImage}
+                alt="Illustration"
+                width={400}
+                height={300}
+                className="max-w-full h-auto"
+              />
+              <Flex direction="col" alignItems="center">
+                <Typography as="h2" size="lg" weight="bold" className="mt-8">
+                  맞춤형 과제 생성을 위한 추가 정보가 필요해요!
+                </Typography>
+                <Typography size="base" weight="normal" className="mt-3">
+                  초기 설정을 통해 여러분에 대해 더 알려주세요!
+                </Typography>
+              </Flex>
+            </Flex>
+            <Flex direction="col" className="flex-[0_0_50%] space-y-4" gap="2">
+              <hr />
+              {children}
+              <hr />
+            </Flex>
+          </Flex>
+        </main>
+      </Flex>
+    </Flex>
+  );
+}
