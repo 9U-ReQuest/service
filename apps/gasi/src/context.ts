5dd185103e707b77f98c629601ffbb26821f8bc1
Hyomin Koo <hyomin@hyperithm.com>
2024-11-22 15:43:15 +0900

    fix(gasi): fastify 로거 사용

--- apps/gasi/src/context.ts
+++ apps/gasi/src/context.ts
@@ -1,21 +1,21 @@
 import { type User, UserSchema } from "@request/specs";
 import type { CreateFastifyContextOptions } from "@trpc/server/adapters/fastify";
 import type { FastifyBaseLogger } from "fastify";
+import { server } from "./index.js";
 import { mUser } from "./model/index.js";
 
 const defaultBaseUrl = process.env.CLIENT_BASE_URL ?? "http://localhost:3000";
 
 export const createContext = async (
   opts: CreateFastifyContextOptions,
-): Promise<{ baseUrl: string; user: User | null; log: FastifyBaseLogger }> => {
-  const log = opts.req.log;
-  log.info(opts.req.headers);
+): Promise<{ baseUrl: string; user: User | null }> => {
+  server.log.info(opts.req.headers);
   const referer = opts.req.headers.referer;
   let user = null;
   let baseUrl = defaultBaseUrl;
   if (opts.req.headers.authorization) {
     const token = opts.req.headers.authorization.split(" ")[1];
-    log.info(token);
+    server.log.info(token);
     const doc = await mUser.findOne({ token });
     if (doc) {
       user = UserSchema.parse(doc.toObject());
@@ -23,12 +23,11 @@ export const createContext = async (
   }
   if (referer) {
     const url = new URL(referer);
-    log.info(url.origin);
+    server.log.info(url.origin);
     baseUrl = url.origin;
   }
   return {
     baseUrl,
     user,
-    log,
   };
 };
