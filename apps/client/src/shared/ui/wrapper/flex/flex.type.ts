3e6d953ab29134c90fb8d1d22656870282dacfc7
mj <minai621@naver.com>
2024-11-22 15:17:15 +0900

    refactor(client): Gap 타입 정의 개선

--- apps/client/src/shared/ui/wrapper/flex/flex.type.ts
+++ apps/client/src/shared/ui/wrapper/flex/flex.type.ts
@@ -6,16 +6,8 @@ type AlignItems = "start" | "end" | "center" | "baseline" | "stretch";
 
 type FlexWrap = "nowrap" | "wrap" | "wrap-reverse";
 
-// Gap<20> = "0" | "1" ... "20"
-// 20부터 0까지 1씩 감소하도록 재귀적으로 타입을 정의
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-type Gap<N extends number, A extends any[] = []> = A["length"] extends N
-  ? `${N}`
-  :
-      | `${
-          // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-          A["length"]
-        }`
-      | Gap<N, [any, ...A]>;
+type Gap<N extends number, Result extends unknown[] = []> = Result["length"] extends N
+  ? `${Result["length"]}`
+  : `${Result["length"]}` | Gap<N, [...Result, unknown]>;
 
 export type { AlignItems, FlexDirection, FlexWrap, Gap, JustifyContent };
