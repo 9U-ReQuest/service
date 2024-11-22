33b5c1813a90a2bc09d5b046afd4294a3891f2b8
mj <minai621@naver.com>
2024-11-22 14:40:44 +0900

    refactor(client): 폰트 경로 수정 및 정리

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -10,47 +10,47 @@ import "./globals.css";
 const sfPro = localFont({
   src: [
     {
-      path: "./fonts/SF-Pro-Display-Black.woff2",
+      path: "../assets/fonts/SF-Pro-Display-Black.woff2",
       weight: "900",
       style: "normal",
     },
     {
-      path: "./fonts/SF-Pro-Display-Heavy.woff2",
+      path: "../assets/fonts/SF-Pro-Display-Heavy.woff2",
       weight: "800",
       style: "normal",
     },
     {
-      path: "./fonts/SF-Pro-Display-Bold.woff2",
+      path: "../assets/fonts/SF-Pro-Display-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./fonts/SF-Pro-Display-Semibold.woff2",
+      path: "../assets/fonts/SF-Pro-Display-Semibold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "./fonts/SF-Pro-Display-Medium.woff2",
+      path: "../assets/fonts/SF-Pro-Display-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts/SF-Pro-Display-Regular.woff2",
+      path: "../assets/fonts/SF-Pro-Display-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/SF-Pro-Display-Light.woff2",
+      path: "../assets/fonts/SF-Pro-Display-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "./fonts/SF-Pro-Display-Thin.woff2",
+      path: "../assets/fonts/SF-Pro-Display-Thin.woff2",
       weight: "200",
       style: "normal",
     },
     {
-      path: "./fonts/SF-Pro-Display-Ultralight.woff2",
+      path: "../assets/fonts/SF-Pro-Display-Ultralight.woff2",
       weight: "100",
       style: "normal",
     },
