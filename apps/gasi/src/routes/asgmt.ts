6d2afaa8fd6b811547590228a2bc241b1c1bb09f
EATSTEAK <me@eatsteak.dev>
2024-11-22 22:23:18 +0900

    feat(gasi): `v1.asgmt.list`

--- apps/gasi/src/routes/asgmt.ts
+++ apps/gasi/src/routes/asgmt.ts
@@ -2,25 +2,28 @@ import { AssignmentFilterSchema, AssignmentPromptSchema } from "@request/specs";
 import { TRPCError } from "@trpc/server";
 import { humanId } from "human-id";
 import { z } from "zod";
-import { createMockAssignment } from "../mockUtils.js";
+import { checkRegistered } from "../auth/token.js";
+import { mAssignment } from "../model/index.js";
 import { p } from "../trpc.js";
 
-export const list = p.input(AssignmentFilterSchema).query(({ input }) => {
-  return {
-    skip: input.skip,
-    limit: input.limit,
-    total: 100,
-    data: Array(Math.min(input.limit, 100 - input.skip)).map((_, i) =>
-      createMockAssignment(
-        humanId({
-          separator: "-",
-          capitalize: false,
-        }),
-        `${input.limit + i}번째 과제`,
-      ),
-    ),
-  };
-});
+export const list = p
+  .input(AssignmentFilterSchema)
+  .query(async ({ input }): Promise<AssignmentListResponse> => {
+    const assignments = await mAssignment.find().limit(input.limit).skip(input.skip).sort({
+      lastUpdated: "asc",
+    });
+    const count = await mAssignment.countDocuments({});
+    const result: AssignmentListResponse = {
+      skip: input.skip,
+      limit: input.limit,
+      total: count,
+      data: assignments.map((doc) => ({
+        ...doc.toObject(),
+        lastUpdated: (doc.lastUpdated as Date).toISOString(),
+      })),
+    };
+    return AssignmentListResponseSchema.parse(result);
+  });
 
 export const get = p.input(z.object({ id: z.string() })).query(({ input }) => {
   if (input.id === "none")
