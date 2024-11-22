3b4c96738c65b794320d704e0de234363c5dbcd1
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-11-23 02:47:05 +0900

    feat(client): 리뷰 결과 페이지 코멘트 제외 API 연동

--- apps/client/src/shared/api/trpc.ts
+++ apps/client/src/shared/api/trpc.ts
@@ -2,13 +2,15 @@ import type { AppRouter } from "@request/specs";
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URI || "http://localhost:8080";
+
 export const trpc = createTRPCNext<AppRouter>({
   config(opts) {
     return {
       ...opts,
       links: [
         httpBatchLink({
-          url: "/trpc",
+          url: `${baseUrl}/trpc`,
           // You can pass any HTTP headers you wish here
         }),
       ],
