24dfe0ceed46bb2ea9d829c67b7d3235d17ae52d
Hyomin Koo <me@eatsteak.dev>
2024-11-24 03:40:49 +0900

    feat(gasi): show container id

--- apps/gasi/src/docker.ts
+++ apps/gasi/src/docker.ts
@@ -1,5 +1,5 @@
 import { TRPCError } from "@trpc/server";
-import { docker } from "./index.js";
+import { docker, server } from "./index.js";
 
 const ECR_REPOSITORY_URL = process.env.ECR_REPOSITORY_URL;
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
@@ -9,39 +9,42 @@ const REQUEST_ENDPOINT = process.env.REQUEST_ENDPOINT;
 const ASSIGNMENT_BUCKET_ID = process.env.ASSIGNMENT_BUCKET_ID;
 const SUBMISSION_BUCKET_ID = process.env.SUBMISSION_BUCKET_ID;
 
-export function makeRepository(userName: string, assignmentId: string, submissionId: string) {
+export async function makeRepository(userName: string, assignmentId: string, submissionId: string) {
   if (!docker || !GITHUB_TOKEN || !ECR_REPOSITORY_URL)
     throw new TRPCError({
       code: "SERVICE_UNAVAILABLE",
       message: "이 서버에서는 github 작업을 지원하지 않습니다.",
     });
   const imageName = "make-repo:latest";
-  docker.createContainer({
+  const env = [
+    `GITHUB_TOKEN=${GITHUB_TOKEN}`,
+    `AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}`,
+    `AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}`,
+    `REQUEST_ENDPOINT=${REQUEST_ENDPOINT}`,
+    `BUCKET_ID=${ASSIGNMENT_BUCKET_ID}`,
+    `USER_LOGIN=${userName}`,
+    `ASSIGNMENT_ID=${assignmentId}`,
+    `SUBMISSION_ID=${submissionId}`,
+  ];
+  server.log.info(env);
+  const container = await docker.createContainer({
     Tty: true,
     Image: imageName,
-    Env: [
-      `GITHUB_TOKEN=${GITHUB_TOKEN}`,
-      `AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}`,
-      `AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}`,
-      `REQUEST_ENDPOINT=${REQUEST_ENDPOINT}`,
-      `BUCKET_ID=${ASSIGNMENT_BUCKET_ID}`,
-      `USER_LOGIN=${userName}`,
-      `ASSIGNMENT_ID=${assignmentId}`,
-      `SUBMISSION_ID=${submissionId}`,
-    ],
+    Env: env,
     HostConfig: {
       NetworkMode: "gasi",
     },
   });
+  server.log.info(`Docker container spawned: ${container.id}`);
 }
-export function submitRepository(submissionId: string) {
+export async function submitRepository(submissionId: string) {
   if (!docker)
     throw new TRPCError({
       code: "SERVICE_UNAVAILABLE",
       message: "이 서버에서는 github 작업을 지원하지 않습니다.",
     });
   const imageName = "submit-repo:latest";
-  docker.createContainer({
+  const container = await docker.createContainer({
     Tty: true,
     Image: imageName,
     Env: [
@@ -55,4 +58,5 @@ export function submitRepository(submissionId: string) {
       NetworkMode: "gasi",
     },
   });
+  server.log.info(`Docker container spawned: ${container.id}`);
 }
