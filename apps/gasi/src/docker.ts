699754141fa15f4535943024360c7ed8e6d8747e
Hyomin Koo <me@eatsteak.dev>
2024-11-23 22:03:43 +0900

    fix(gasi): split assignment bucket and submission bucket

--- apps/gasi/src/docker.ts
+++ apps/gasi/src/docker.ts
@@ -6,7 +6,8 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 const REQUEST_ENDPOINT = process.env.REQUEST_ENDPOINT;
-const BUCKET_ID = process.env.BUCKET_ID;
+const ASSIGNMENT_BUCKET_ID = process.env.ASSIGNMENT_BUCKET_ID;
+const SUBMISSION_BUCKET_ID = process.env.SUBMISSION_BUCKET_ID;
 
 export function makeRepository(userName: string, assignmentId: string, submissionId: string) {
   if(!docker) throw new TRPCError({ code: "SERVICE_UNAVAILABLE", message: "이 서버에서는 github 작업을 지원하지 않습니다." });
@@ -19,7 +20,7 @@ export function makeRepository(userName: string, assignmentId: string, submissio
       `AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}`,
       `AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}`,
       `REQUEST_ENDPOINT=${REQUEST_ENDPOINT}`,
-      `BUCKET_ID=${BUCKET_ID}`,
+      `BUCKET_ID=${ASSIGNMENT_BUCKET_ID}`,
       `USER_LOGIN=${userName}`,
       `ASSIGNMENT_ID=${assignmentId}`,
       `SUBMISSION_ID=${submissionId}`,
@@ -39,7 +40,7 @@ export function submitRepository(submissionId: string) {
       `GITHUB_TOKEN=${GITHUB_TOKEN}`,
       `AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}`,
       `AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}`,
-      `BUCKET_ID=${BUCKET_ID}`,
+      `BUCKET_ID=${SUBMISSION_BUCKET_ID}`,
       `SUBMISSION_ID=${submissionId}`,
     ],
     HostConfig: {
